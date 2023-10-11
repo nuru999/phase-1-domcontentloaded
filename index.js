48cb1c695051d3125bcd10e1a1885615bc5deb56
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-10-11 13:10:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// Your code goes here //
+document.addEventListener('DOMContentLoaded', function() {
+ let paragraph = document.querySelector('p');
+    if (paragraph) {
+      paragraph.textContent = "This is really cool!";
+    }
+  });
